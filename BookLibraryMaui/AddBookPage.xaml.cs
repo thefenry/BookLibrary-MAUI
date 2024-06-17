1ac13832a6d61b2f1c1736d4c9e0ae33a247ac4c
Ivan Sued <ivan@practicepanther.com>
2024-06-16 21:42:05 -0700

    Adding addBookPage

--- BookLibraryMaui/AddBookPage.xaml.cs
+++ BookLibraryMaui/AddBookPage.xaml.cs
@@ -2,8 +2,36 @@ namespace BookLibraryMaui;
 
 public partial class AddBookPage : ContentPage
 {
-	public AddBookPage()
+    private double _mySliderValue = 0.25;
+    public double MySliderValue
+    {
+        get => _mySliderValue;
+        set
+        {
+            if (_mySliderValue != value)
+            {
+                _mySliderValue = value;
+                OnPropertyChanged(); // Notify the UI
+            }
+        }
+    }
+
+
+    public AddBookPage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = this;
+    }
+
+    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        // Snap the value to the nearest multiple of 0.25
+        var newValue = Math.Round(e.NewValue / 0.25) * 0.25;
+        MySliderValue = Math.Clamp(newValue, 0, 5); // Ensure it stays within the valid range       
+    }   
+
+    private void OnScan_Clicked(object sender, EventArgs env)
+    {
+        
+    }
 }
